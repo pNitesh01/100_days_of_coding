57e98d1c8450001822c99e25985d2f70701e9438
pNitesh01 <patwanitesh1997@gmail.com>
2022-06-01 17:17:37 +0530

    Add etch a sketch and turtle race games.

---
+++ Intermediate_level/DAY-19/etch-a-sketch/main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+    
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    current_heading = tim.heading()
+    tim.setheading(current_heading + 10)
+
+def turn_right():
+    current_heading = tim.heading()
+    tim.setheading(current_heading - 10)
+
+def clear_drawing():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+    
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear_drawing)
+screen.exitonclick()
