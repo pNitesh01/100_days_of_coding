a80146c825ff7b95eef2b59ef6dd44aaf9c16c3e
pNitesh01 <patwanitesh1997@gmail.com>
2022-06-06 22:50:11 +0530

    Add search functionality and handle exceptions

--- Intermediate_level/DAY-30/password-manager/main.py
+++ Intermediate_level/DAY-30/password-manager/main.py
@@ -28,7 +28,7 @@ def generate_password():
     
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = website_entry.get()
+    website = website_entry.get().title()
     username = email_entry.get()
     password = password_entry.get()
     new_data = {
@@ -63,6 +63,25 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get().title()
+    
+    try:
+        with open(FILE, 'r') as data_file:
+            data = json.load(data_file)
+    
+    except FileNotFoundError:
+        messagebox.showerror(message="No Data File Found")
+    
+    else:   
+        if website in data:
+            username = data[website]['username']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f"Username: {username}\nPassword: {password}")
+        else:
+            messagebox.showerror(title="Error", message=f"No details for {website} exists.")
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -86,7 +105,7 @@ password_label.grid(row=3, column=0)
 
 #Entries
 website_entry = Entry()
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.focus()
 
 email_entry = Entry()
@@ -97,6 +116,9 @@ password_entry = Entry()
 password_entry.grid(row=3, column=1, sticky="EW")
 
 #Buttons
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, sticky="EW")
+
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2, sticky="EW")
 
